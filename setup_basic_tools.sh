9c2dbf6d15942828bf0cc15a225d7853e96c22e5
Artsiom Kolas <61182975+kolasdevpy@users.noreply.github.com>
2026-04-15 15:26:55 +0300

    Update setup_basic_tools.sh

--- setup_basic_tools.sh
+++ setup_basic_tools.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # BASIC TOOLS SETUP FOR UBUNTU 24.04
 # Installs: git, htop, tree, zip, unzip, gzip
 
-# wget https://raw.githubusercontent.com/kolasdevpy/set-ubuntu22.04/setup_basic_tools.sh
+# wget https://raw.githubusercontent.com/kolasdevpy/set-ubuntu22.04/main/setup_basic_tools.sh
 # chmod +x setup_basic_tools.sh
 # sudo setup_basic_tools.sh
 # ============================================================
