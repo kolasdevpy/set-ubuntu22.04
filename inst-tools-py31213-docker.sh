cb535f33ae91cc056a9870d9a8b1f0a2a39ef441
Artsiom Kolas <61182975+kolasdevpy@users.noreply.github.com>
2026-04-17 11:47:24 +0300

    Update inst-tools-py31213-docker.sh

--- inst-tools-py31213-docker.sh
+++ inst-tools-py31213-docker.sh
@@ -68,28 +68,32 @@ export PATH="$HOME/.local/bin:$PATH"
 
 poetry --version
 
-# 5. Install Docker (system-wide, using sudo)
+# DOCKER
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add Docker repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"${UBUNTU_CODENAME:-$VERSION_CODENAME}\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
+# Install the latest version
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Add user to docker group (to avoid using sudo)
-sudo usermod -aG docker "$CURRENT_USER"
+sudo usermod -aG docker "$USER"
+
 sudo systemctl restart docker
 
-# Verify Docker
 docker --version
 docker compose version
 
 echo "------------------------------"
-echo "✅ Installation completed for user $CURRENT_USER"
-echo "⚠️  To apply the docker group change:         newgrp docker"
-echo "⚠️  To reload .bashrc settings:               source ~/.bashrc  (or restart your terminal)"
+echo "⚠️  run: sudo usermod -aG docker admin"
+echo "⚠️  run: newgrp docker"
+echo "⚠️  Reload the terminal"
